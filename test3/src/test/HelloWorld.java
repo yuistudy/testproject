ef8d6cb74ba7fe26caf4e2c9c616de809e3e76e6
yui <tetuya120@hotmail.com>
2011-07-14 15:57:20 +0900

    commit test

---
+++ test3/src/test/HelloWorld.java
@@ -0,0 +1,11 @@
+package test;
+
+public class HelloWorld {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("HELLO WORLD !!");
+    }
+}
